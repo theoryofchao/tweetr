c8a0e6375fa5fefdf91ed349a8ac35bc388cef65
theoryofchao <roger.ti.chao@gmail.com>
2017-01-25 14:05:15 -0500

    added notification and cleaned up code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,80 +5,86 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
+var data = [];
+
+let createTweetElement = (tweetData) => {
+  let $article = $("<article>", {"class": "tweet"});
+  let $header = $("<header>");
+  let $tweetpic = $("<div>", {"class": "tweet-pic"});
+  let $img = $("<img>");
+  let $username = $("<div>", {"class": "tweet-username"}).text(tweetData.user.name);
+  let $userhandle = $("<div>", {"class": "tweet-userhandle"}).text(tweetData.user.handle);
+  let $content = $("<div>", {"class": "tweet-body"}).text(tweetData.content.text);
+  let $footer = $("<footer>");
+  let date = new Date(tweetData.created_at);
+  let $date = $("<div>", {"class": "tweet-date"}).text(date.toDateString());
+  let $symbols = $("<div>", {"class": "tweet-icons"})
+        .append($("<i>", {"class": "fa fa-flag"}))
+        .append($("<i>", {"class": "fa fa-retweet"}))
+        .append($("<i>", {"class": "fa fa-heart"}));
+  $img.attr("src", tweetData.user.avatars.small);
+  $tweetpic.append($img);
+  $header.append($tweetpic).append($username).append($userhandle);
+  $footer.append($date).append($symbols);
+  $article.append($header).append($content).append($footer);
+  return $article;
+};
+
+let renderTweets = (arrayTweets) => {
+  arrayTweets.forEach( (tweet) => {
+    $('#tweets-container').append(createTweetElement(tweet));
+  });
+};
+
+let loadTweets = () => {
+  $.get("/tweets/").then(
+    (data) => {
+      renderTweets(data);
+    },
+    () => {
+      console.log("SOmething went wrong");
+    }
+  );
+};
+
+let tweetNotification = (message) => {
+  $("#tweet-notification").text(message).fadeTo(2000, 1, () => {
+    $("#tweet-notification").fadeTo(2000, 0);       
+  });
+};
+
 $( () => {
-  var data = [];
-
-  let createTweetElement = (tweetData) => {
-    let $article = $("<article>", {"class": "tweet"});
-    let $header = $("<header>");
-    let $tweetpic = $("<div>", {"class": "tweet-pic"});
-    let $img = $("<img>");
-    let $username = $("<div>", {"class": "tweet-username"}).text(tweetData.user.name);
-    let $userhandle = $("<div>", {"class": "tweet-userhandle"}).text(tweetData.user.handle);
-    let $content = $("<div>", {"class": "tweet-body"}).text(tweetData.content.text);
-    let $footer = $("<footer>");
-    let date = new Date(tweetData.created_at);
-    let $date = $("<div>", {"class": "tweet-date"}).text(date.toDateString());
-    let $symbols = $("<div>", {"class": "tweet-icons"})
-          .append($("<i>", {"class": "fa fa-flag"}))
-          .append($("<i>", {"class": "fa fa-retweet"}))
-          .append($("<i>", {"class": "fa fa-heart"}));
-    $img.attr("src", tweetData.user.avatars.small);
-    $tweetpic.append($img);
-    $header.append($tweetpic).append($username).append($userhandle);
-    $footer.append($date).append($symbols);
-    $article.append($header).append($content).append($footer);
-    return $article;
-  };
-
-  let renderTweets = (arrayTweets) => {
-    arrayTweets.forEach( (tweet) => {
-      $('#tweets-container').append(createTweetElement(tweet));
-    });
-  };
-
-  let loadTweets = () => {
-    $.ajax({
-      type: 'GET',
-      url: '/tweets/',
-      error: () => {
-        console.log("Something went wrong");
-      },
-      success: (data) => {
-        renderTweets(data);
-      }
-    });
-  };
-
   loadTweets();
 
   //Ajax For Getting More Tweets
-  $('main .new-tweet .submit-new-tweet').on('click', (event) => {
+  $('main .new-tweet').on('submit', (event) => {
     //console.log('Performing Ajax Call');
     event.preventDefault();
     let text = $('main .new-tweet .new-tweet-input').val();
     if(text.length === 0) {
-      //TODO: implement flash message
-      alert("Tweet cannot be empty!");
+      tweetNotification("Tweet cannot be empty!");
       return false;
     }
     else if(text.length > 140) {
-      //TODO: implement flash message
-      alert("Tweet cannot exceed 140 characters!");
+      tweetNotification("Tweet cannot exceed 140 characters!");
       return false;
     }
 
-    $.ajax({
-      type: 'POST',
-      url: '/tweets/',
-      data: {'text': text},
-      success: (data) => {
+    $.post("/tweets/", { text: text }).then(
+      (data) => {
         $('main .new-tweet .new-tweet-input').val('');
         $('main .counter').text(140);
+        //TODO: update to this after I sort the items by date
+        //$('#tweets-container').empty();
+        //loadTweets(); 
         $('#tweets-container').prepend(createTweetElement(data));
-        console.log(data);
+        tweetNotification("Tweeted!!");
+      },
+      () => {
+        //Flash box with error
+        tweetNotification("Something went wrong :(");
       }
-    });
+    );
   });
 
   $('.compose').on('click', (event) => {
